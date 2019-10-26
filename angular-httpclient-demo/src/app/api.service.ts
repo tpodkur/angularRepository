8ade558c655c5341c3d49366bab51e257764acd3
Таня <t.podkur@g.nsu.ru>
2019-10-27 00:11:03 +0700

    add post-request to api-service

--- angular-httpclient-demo/src/app/api.service.ts
+++ angular-httpclient-demo/src/app/api.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Student } from './student'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  API_KEY = '2f3f8f28fef846258b231cff488cfea4';
 
-public getStudents(){
-  return this.httpClient.get(`/api`);
-}
+  public getStudents(){
+    return this.httpClient.get(`/api`);
+  }
   constructor(private httpClient: HttpClient) { }
+
+  public postStudent(student: Student){
+    const body = {firstName: student.firstName, lastName: student.lastName, id: student.id};
+    return this.httpClient.post('/api', body); 
+  }
 }
--- angular-httpclient-demo/src/app/student-editor/student-editor.component.ts
+++ angular-httpclient-demo/src/app/student-editor/student-editor.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { ApiService } from '../api.service';
+import { Student } from '../student';
 
 @Component({
   selector: 'app-student-editor',
@@ -7,14 +9,16 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./student-editor.component.css']
 })
 export class StudentEditorComponent {
-  studentForm = new FormGroup({
+    studentForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
   });
 
+  constructor(private apiService: ApiService) { }
+
   onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.warn(this.studentForm.value);
+    const student : Student = this.studentForm.value;
+    console.log(student);
+    this.apiService.postStudent(student);
   }
 }
-
